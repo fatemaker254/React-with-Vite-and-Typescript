cbcd9b65df322107e06b17564628ead8b43dba40
fatemaker254 <dassoham254@gmail.com>
2023-09-19 21:56:59 +0530

    Fragments

--- GamerStop/src/components/ListGroup.tsx
+++ GamerStop/src/components/ListGroup.tsx
@@ -1,15 +1,19 @@
 
+import {Fragment} from "react"; //to use multiple elements in a component we can use fragments
 //To create a ListGroup component
 function ListGroup(){
     return (
-    <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fourth item</li>
-    </ul>
+    <Fragment>//wrap inside a fragment
+        <h1>LIST GROUP</h1>
+        <ul className="list-group">
+           <li className="list-group-item">An item</li>
+           <li className="list-group-item">A second item</li>
+           <li className="list-group-item">A third item</li>
+           <li className="list-group-item">A fourth item</li>
+           <li className="list-group-item">And a fourth item</li>
+        </ul>
+    </Fragment>
     );
 }
-
+//we can also use fragments by just giving <> </> instead of <Fragment> </Fragment>
 export default ListGroup;
