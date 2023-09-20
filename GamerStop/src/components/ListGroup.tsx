cf0f97f31a3c6281b6934538bce5ea8d04f523ed
fatemaker254 <dassoham254@gmail.com>
2023-09-20 19:17:45 +0530

    Mapping items

--- GamerStop/src/components/ListGroup.tsx
+++ GamerStop/src/components/ListGroup.tsx
@@ -1,9 +1,10 @@
 
 import {Fragment} from "react"; //to use multiple elements in a component we can use fragments
 //To create a ListGroup component
-function ListGroup(){
+{/* function ListGroup(){
     return (
-    <Fragment>//wrap inside a fragment
+    <Fragment>
+        /* wrap inside a fragment
         <h1>LIST GROUP</h1>
         <ul className="list-group">
            <li className="list-group-item">An item</li>
@@ -11,9 +12,24 @@ function ListGroup(){
            <li className="list-group-item">A third item</li>
            <li className="list-group-item">A fourth item</li>
            <li className="list-group-item">And a fourth item</li>
-        </ul>
+    </ul> 
     </Fragment>
+    ); 
+} */}
+//we can also use fragments by just giving <> </> instead of <Fragment> </Fragment>
+//we are using <> and mapping as follows
+
+function ListGroup(){
+    const items=["INDIA","AUS","ENG","SA","PAK"]
+    return (
+        <>
+        <h1>LIST GROUP</h1>
+        <ul className="listgroup">
+            {items.map((item)=>( //we are using the mapping method here
+                <li key={item}>{item}</li> //ket is necessary for react
+            ))}
+        </ul>
+        </>
     );
 }
-//we can also use fragments by just giving <> </> instead of <Fragment> </Fragment>
 export default ListGroup;
